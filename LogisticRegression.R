d2fe6850c79854ad7064786bf607d7d9d26a1509
Lindsey Brooks <lindseybrooks@Lindseys-Air.attlocal.net>
2019-11-30 18:00:31 -0600

    Add changes

--- LogisticRegression.R
+++ LogisticRegression.R
@@ -3,24 +3,27 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 library(rmarkdown)
-library(ggplot2)
 library(cowplot)
 library(caret)
+library(data.table)
+library(rattle)
+library(rpart.plot)
 
-# Loading NACC data set 
+##########################################
+# Preprocessing of data
+
+
+# Loading raw NACC data set 
 NACC <- read.csv(file = "/Users/lindseybrooks/Desktop/Practicum/Shi05082017-NACC.csv",
                  header = TRUE,
                  stringsAsFactors = FALSE)
 
 # New data set from Variable selection
-Data_Subset <- select(NACC, VISITYR,BIRTHYR,SEX,EDUC,NACCBMI,SMOKYRS,PACKSPER,ALCOHOL,NACCUDSD,CVHATT,CBSTROKE,HYPERTEN,DEP2YRS,NACCAPOE)
+Data_Subset <- select(NACC, NACCID, VISITYR,BIRTHYR,SEX,EDUC,NACCBMI,SMOKYRS,PACKSPER,ALCOHOL,NACCUDSD,CVHATT,CBSTROKE,HYPERTEN,DEP2YRS,NACCAPOE)
 
 
 # Adding 'AGE' variable calculated from VISITYR and BIRTHYR variables
 Data_Subset <- mutate(Data_Subset, AGE = VISITYR - BIRTHYR)
-
-# Remove VISITYR and BIRTHYR now we have age
-Data_Subset <- Data_Subset[-c(1, 2)]
 
 # Change 'SEX' numeric variable to be a factor
 Data_Subset$SEX <- factor(Data_Subset$SEX, levels = c(1, 2), labels = c("Male", "Female"))
@@ -33,17 +36,20 @@ Data_Subset <- filter(Data_Subset, Data_Subset$EDUC <= 25)
 Data_Subset$EDUC <- cut(Data_Subset$EDUC, breaks=c(0, 11, 15, 17, 19, 25), labels=c("Below high school or GRE", "high school or GRE", "bachelor’s degree", "master's degree", "doctorate"))
 
 # Cleaning 'NACCBMI' variable.
-Data_Subset <- filter(Data_Subset, Data_Subset$NACCBMI != "888.8", Data_Subset$NACCBMI != "-4" )
+Data_Subset <- filter(Data_Subset, Data_Subset$NACCBMI != 888.8, Data_Subset$NACCBMI != -4 )
 
 # In 'SMOKYRS' numeric variable, replace values "88" to "NA", and "99" and "-4" to "Unknown"
-Data_Subset$SMOKYRS[Data_Subset$SMOKYRS == 88] <- 0
+Data_Subset$SMOKYRS[Data_Subset$SMOKYRS == 88] <- 0  # 88 is not applicable, assume this is 0
 Data_Subset$SMOKYRS[Data_Subset$SMOKYRS == 99] <- NA
 Data_Subset$SMOKYRS[Data_Subset$SMOKYRS == -4] <- NA
 
-# Change 'PACKSPER' numeric variable to be a factor
+# Clean PACKSPER
+Data_Subset$PACKSPER[Data_Subset$PACKSPER == 8] <- 0 # 8 is not applicable, assumet this is 0
 Data_Subset$PACKSPER[Data_Subset$PACKSPER == 9] <- NA
 Data_Subset$PACKSPER[Data_Subset$PACKSPER == -4] <- NA
-Data_Subset$PACKSPER <- factor(Data_Subset$PACKSPER, levels = c(0, 1, 2, 3, 4, 5), labels = c("No reported cigarette use", "1 cigarette to less than 1/2 pack", "½ pack to less than 1 pack", "1 pack to 1½ packs", "1½ packs to 2 packs", "More than two packs"))
+
+# Create a smoking density variable
+Data_Subset <- mutate(Data_Subset, SMOKER_DENSITY = SMOKYRS * PACKSPER)
 
 # Change 'ALCOHOL' numeric variable to be a factor, and replace invalid entries to 'NA'
 Data_Subset$ALCOHOL <- factor(Data_Subset$ALCOHOL,levels=c(0:2),labels=c("Absent","Active","Inactive"))
@@ -75,25 +81,32 @@ Data_Subset$NACCAPOE <- factor(Data_Subset$NACCAPOE, levels=c(1,2,3,4,5,6),label
 Data_Subset$NACCAPOE[Data_Subset$NACCAPOE == 9] <- NA
 Data_Subset$NACCAPOE[Data_Subset$NACCAPOE == -4] <- NA
 
-
-View(Data_Subset)
-write.csv(Data_Subset, file = "Data_Subset.csv")
 summary(Data_Subset)
 
 # Remove instances with NA values
-nrow(Data_Subset) # 43086 rows
-Data_Subset <- Data_Subset[!(is.na(Data_Subset$SMOKYRS) | 
-                               is.na(Data_Subset$EDUC) |
-                               is.na(Data_Subset$PACKSPER) |
+nrow(Data_Subset) 
+Data_Subset <- Data_Subset[!(is.na(Data_Subset$EDUC) |
+                               is.na(Data_Subset$SMOKER_DENSITY) |
                                is.na(Data_Subset$ALCOHOL) |
                                is.na(Data_Subset$CVHATT) |
                                is.na(Data_Subset$CBSTROKE) |
                                is.na(Data_Subset$HYPERTEN) |
                                is.na(Data_Subset$DEP2YRS) |
+                               is.na(Data_Subset$NACCBMI) |
                                is.na(Data_Subset$NACCAPOE)
 ),]
-nrow(Data_Subset)  # 32117 rows
+nrow(Data_Subset) 
 summary(Data_Subset)
+
+# check for duplicated survey results by same participant
+aggregate(data.frame(count = Data_Subset$NACCID), list(value = Data_Subset$NACCID), length)
+
+# we only want the latest observation from the participant 
+Data_Subset <- setDT(Data_Subset)[,.SD[which.max(VISITYR)],keyby=NACCID]
+
+
+##########################################
+# Descriptive Analysis
 
 hist(Data_Subset$AGE)
 hist(Data_Subset$NACCBMI)
@@ -101,51 +114,39 @@ boxplot(NACCBMI~NACCUDSD,data=Data_Subset, xlab="NACCUDSD", ylab="NACCBMI")
 boxplot(AGE~NACCUDSD,data=Data_Subset, xlab="NACCUDSD", ylab="AGE")
 
 
-## Quality control by making sure all of the factor
-## levels are represented by people with and without Alzheimers
-xtabs(~ NACCUDSD + SEX, data=Data_Subset)
-xtabs(~ NACCUDSD + EDUC, data=Data_Subset)
-xtabs(~ NACCUDSD + NACCBMI, data=Data_Subset)
-xtabs(~ NACCUDSD + SMOKYRS, data=Data_Subset)
-xtabs(~ NACCUDSD + PACKSPER, data=Data_Subset)
-xtabs(~ NACCUDSD + ALCOHOL, data=Data_Subset)
-xtabs(~ NACCUDSD + CVHATT, data=Data_Subset)
-xtabs(~ NACCUDSD + CBSTROKE, data=Data_Subset)
-xtabs(~ NACCUDSD + HYPERTEN, data=Data_Subset)
-xtabs(~ NACCUDSD + DEP2YRS, data=Data_Subset)
-xtabs(~ NACCUDSD + NACCAPOE, data=Data_Subset)
-xtabs(~ NACCUDSD + CBSTROKE, data=Data_Subset)
-xtabs(~ NACCUDSD + AGE, data=Data_Subset)
 
+##########################################
+# Predictive Analysis
 
 # remap levels for binomial
 Data_Subset$NACCUDSD <- mapvalues(Data_Subset$NACCUDSD, from = c("Impaired", "MCI", "Dementia"), to = c("Abnormal", "Abnormal", "Abnormal"))
-
 
 # split data into training and test, based on values of dependent variable 
 trainIndex <- createDataPartition(Data_Subset$NACCUDSD, p = .75,list=FALSE)
 training <- Data_Subset[trainIndex,]
 testing <- Data_Subset[-trainIndex,]
 trCntl <- trainControl(method = "CV",number = 5)
-glmModel <- train(NACCUDSD ~ .,data = training,trControl = trCntl,method="glm",family = "binomial")
-
-
+glmModel <- train(NACCUDSD ~ SEX + EDUC + NACCBMI + ALCOHOL + CVHATT + CBSTROKE + HYPERTEN + DEP2YRS + NACCAPOE + AGE + SMOKER_DENSITY,data = training,trControl = trCntl,method="glm",family = "binomial")
 summary(glmModel)
-glmModel
-
 
 trainPredicted <- predict(glmModel,testing)
 
 confusionMatrix(trainPredicted,reference=testing$NACCUDSD)
 
-# plot the ROC curve and calculate the AUC (area under the curve) which are 
-# typical performance measurements for a binary classifier.
-# The ROC is a curve generated by plotting the true positive rate (TPR) 
-# against the false positive rate (FPR) at various threshold settings while 
-# the AUC is the area under the ROC curve. As a rule of thumb, a model with 
-# good predictive ability should have an AUC closer to 1 (1 is ideal) than to 0.5.
+# Decision Trees
+dtrees_fit <- rpart(NACCUDSD ~ SEX + EDUC + NACCBMI + ALCOHOL + CVHATT + CBSTROKE + HYPERTEN + DEP2YRS + NACCAPOE + AGE + SMOKER_DENSITY, data=training,
+             method="class")
+dtrees_fit # basic model results
+
+summary(dtrees_fit)
+
+par(mar = c(5,4,1,2)) #setting the margins
+fancyRpartPlot(dtrees_fit, sub = NULL, main = "Final Decision Tree")
+
+##########################################
+# ROC Curve
 library(ROCR)
-model <- glm(NACCUDSD ~.,family=binomial(link='logit'),data=training)
+model <- glm(NACCUDSD ~ SEX + EDUC + NACCBMI + ALCOHOL + CVHATT + CBSTROKE + HYPERTEN + DEP2YRS + NACCAPOE + AGE + SMOKER_DENSITY,family=binomial(link='logit'),data=training)
 p <- predict(model, newdata=subset(testing,select=c(1,2,3,4,5,6,8,9,10,11,12,13)), type="response")
 pr <- prediction(p, testing$NACCUDSD)
 prf <- performance(pr, measure = "tpr", x.measure = "fpr")
@@ -154,4 +155,8 @@ plot(prf)
 auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
 auc
+
+anova(model, test="Chisq")
+
+
 
